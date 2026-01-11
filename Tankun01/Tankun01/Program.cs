d4b3b01bde749304286839d96840017c0a0e05b3
Tankun888 <taenkhun.t@kkumail.com>
2026-01-11 10:23:20 +0700

    TaenkhunLab

---
+++ Tankun01/Tankun01/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Tankun01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tankun machine = new Tankun(2000, 500, 300, 300);
+
+            while (true)
+            {
+                Console.WriteLine("\n======================");
+                Console.WriteLine(" Coffee Machine Menu ");
+                Console.WriteLine("======================");
+                Console.WriteLine("1. Black Coffee");
+                Console.WriteLine("2. Mocha");
+                Console.WriteLine("3. Latte");
+                Console.WriteLine("4. Chocolate");
+                Console.WriteLine("5. Refill Stock");
+                Console.WriteLine("6. Show Stock");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select menu: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        machine.MakeDrink(300, 20, 0, 0, "Black Coffee");
+                        break;
+
+                    case "2":
+                        machine.MakeDrink(300, 20, 0, 10, "Mocha");
+                        break;
+
+                    case "3":
+                        machine.MakeDrink(300, 20, 10, 0, "Latte");
+                        break;
+
+                    case "4":
+                        machine.MakeDrink(300, 0, 0, 20, "Chocolate");
+                        break;
+
+                    case "5":
+                        Console.Write("Add water: ");
+                        int w = int.Parse(Console.ReadLine());
+                        Console.Write("Add coffee: ");
+                        int c = int.Parse(Console.ReadLine());
+                        Console.Write("Add milk: ");
+                        int m = int.Parse(Console.ReadLine());
+                        Console.Write("Add chocolate: ");
+                        int ch = int.Parse(Console.ReadLine());
+                        machine.Refill(w, c, m, ch);
+                        break;
+
+                    case "6":
+                        machine.ShowStock();
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Exit program");
+                        return;
+
+                    default:
+                        Console.WriteLine("Wrong input, try again");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Tankun01/Tankun01/Tankun.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Tankun01
+{
+    internal class Tankun
+    {
+        // วัตถุดิบ (กรัม)
+        private int water;
+        private int coffee;
+        private int milk;
+        private int chocolate;
+
+        // constructor
+        public Tankun(int water, int coffee, int milk, int chocolate)
+        {
+            this.water = water;
+            this.coffee = coffee;
+            this.milk = milk;
+            this.chocolate = chocolate;
+        }
+
+        // ชงเครื่องดื่ม
+        public void MakeDrink(int w, int c, int m, int ch, string name)
+        {
+            if (water >= w && coffee >= c && milk >= m && chocolate >= ch)
+            {
+                water -= w;
+                coffee -= c;
+                milk -= m;
+                chocolate -= ch;
+
+                Console.WriteLine($"กำลังชง {name} ... เสร็จแล้ว ☕");
+            }
+            else
+            {
+                Console.WriteLine("วัตถุดิบไม่เพียงพอ ไม่สามารถชงได้");
+            }
+        }
+
+        // แสดงสต็อก
+        public void ShowStock()
+        {
+            Console.WriteLine("---- Stock ----");
+            Console.WriteLine("Water      : " + water + " g");
+            Console.WriteLine("Coffee     : " + coffee + " g");
+            Console.WriteLine("Milk       : " + milk + " g");
+            Console.WriteLine("Chocolate  : " + chocolate + " g");
+        }
+
+        // เติมวัตถุดิบ
+        public void Refill(int w, int c, int m, int ch)
+        {
+            water += w;
+            coffee += c;
+            milk += m;
+            chocolate += ch;
+
+            Console.WriteLine("Refill completed");
+            ShowStock();
+        }
+    }
+}
